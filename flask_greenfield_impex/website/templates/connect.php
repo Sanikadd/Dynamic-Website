b0049835c34e0f1e9a6830732290aa3593a6d1af
Sanika Desai <sanikadesai17@gmail.com>
2023-04-12 23:07:41 +0530

    12 april 23

---
+++ flask_greenfield_impex/website/templates/connect.php
@@ -0,0 +1,24 @@
+<?php
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $emailId = $_POST['emailId'];
+    $mobileNumber = $_POST['mobileNumber'];
+    $messageSend = $_POST['messageSend'];
+
+    //database connection
+    $conn = new mysqli('localhost','root','','test1');
+    if ($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }
+    else{
+        $stmt = $conn->prepare("insert into contactus(firstName, lastName, emailId, mobileNumber, messageSend) values (?,?,?,?,?)");
+        $stmt->bind_param("sssis",$firstName, $lastName, $emailId, $mobileNumber, $messageSend);
+        $stmt->execute();
+        echo "Message sent....";
+        echo "</br>";
+        echo "</br>";
+        echo "<button><a href='index.php'>Return to home page</a></button>";
+        $stmt->close();
+        $conn->close();
+    }
+?>
